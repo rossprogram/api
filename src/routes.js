761db18e15f6976571246c2cc89cfcdd7497d471
Jim Fowler <fowler@math.osu.edu>
2020-01-02 09:47:19 -0500

    less rate limiting

--- src/routes.js
+++ src/routes.js
@@ -20,10 +20,10 @@ router.use(apiLimiter);
 router.use(fileUpload());
 
 const createAccountLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 1 hour window
-  max: 5, // start blocking after 5 requests
+  windowMs: 15 * 60 * 1000, // 15 minute window
+  max: 10, // start blocking after 10 requests
   message:
-    'Too many accounts created from this IP; please try again after an hour',
+    'Too many accounts created from this IP; please try again later.',
 });
 router.post('/users/:user', createAccountLimiter, userController.post);
 
